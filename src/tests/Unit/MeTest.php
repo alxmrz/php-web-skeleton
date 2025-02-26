56be0af0a98f45db955bc7f9623eed2ff988f54d
alxmrz <alexandr.moroz97@mail.ru>
2025-02-26 20:31:43 +0300

    fix xdebug connection with phpstorm, added tests templates

---
+++ src/tests/Unit/MeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use Codeception\PHPUnit\TestCase;
+
+class MeTest extends TestCase
+{
+    public function testMe()
+    {
+        $this->assertTrue(true);
+    }
+}
